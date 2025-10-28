516b27e2ae2ad9a6fe09e8d84112618cee99e313
Noemiss13 <zavalanoemi100@gmail.com>
2025-10-28 11:42:36 -0600

    se realizo una nueva prueba agregando un test para suma

--- test/test_app.py
+++ test/test_app.py
@@ -17,27 +17,3 @@ def test_sumar(cliente):
     assert r.status_code == 200
     assert r.get_json()['resultado'] == 5
 
-def test_restar(cliente):
-    r = cliente.get('/restar?a=10&b=4')
-    assert r.status_code == 200
-    assert r.get_json()['resultado'] == 6
-
-def test_multiplicar(cliente):
-    r = cliente.get('/multiplicar?a=3&b=5')
-    assert r.status_code == 200
-    assert r.get_json()['resultado'] == 15
-
-def test_dividir(cliente):
-    r = cliente.get('/dividir?a=10&b=2')
-    assert r.status_code == 200
-    assert r.get_json()['resultado'] == 5
-
-def test_dividir_por_cero(cliente):
-    r = cliente.get('/dividir?a=10&b=0')
-    assert r.status_code == 400
-    assert 'error' in r.get_json()
-
-def test_sumar_negativos(cliente):
-    r = cliente.get('/sumar?a=-5&b=-3')
-    assert r.status_code == 200
-    assert r.get_json()['resultado'] == -8
